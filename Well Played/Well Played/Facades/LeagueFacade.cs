0911a915e8dc7ae4ca475d9babffd2e34136f521
thekinghippo <mark.young1@live.com>
2015-12-14 02:03:37 -0500

    Update LeagueFacade.cs

--- Well Played/Well Played/Facades/LeagueFacade.cs
+++ Well Played/Well Played/Facades/LeagueFacade.cs
@@ -12,7 +12,7 @@ namespace Well_Played.Facades
 {
     class LeagueFacade
     {
-        private const string apikey = "b2d88c91-7e1f-4ffb-87b9-6052306595c4";
+        private const string apikey = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
         //private static Champion result;
 
 
