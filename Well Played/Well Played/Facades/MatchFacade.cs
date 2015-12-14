b51ca2fd185c357cd03643c2005639ba64977387
thekinghippo <mark.young1@live.com>
2015-12-14 02:03:59 -0500

    Update MatchFacade.cs

--- Well Played/Well Played/Facades/MatchFacade.cs
+++ Well Played/Well Played/Facades/MatchFacade.cs
@@ -12,7 +12,7 @@ namespace Well_Played.Facades
 {
     class MatchFacade
     {
-        private const string apikey = "b2d88c91-7e1f-4ffb-87b9-6052306595c4";
+        private const string apikey = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
 
         public static async Task<Well_Played.Models.MatchList> GetMatchList( string SummonerID )
         {
